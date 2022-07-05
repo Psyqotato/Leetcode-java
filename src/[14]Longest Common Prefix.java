c04b736c2e9f85775b5050a40abeb4fe4238e439
Potato <potatomichael@gmail.com>
2022-07-06 08:54:45 +1000

    new

--- src/[14]Longest Common Prefix.java
+++ src/[14]Longest Common Prefix.java
@@ -1,12 +1,12 @@
 class Solution {
     public String longestCommonPrefix(String[] strs) {
         String result = "";
-        for (int i = 1; i <= strs[0].length(); i++) {
-            result = strs[0].substring(0,i);
+        for (int i = 0; i < strs[0].length(); i++) {
             for (int j = 1; j < strs.length; j++) {
-                if (i > strs[j].length()) return result.substring(0,i-1);
-                if (!result.equals(strs[j].substring(0,i))) return result.substring(0,i-1);
+                if (i >= strs[j].length()) return result;
+                if (strs[0].charAt(i) != strs[j].charAt(i)) return result;
             }
+            result = strs[0].substring(0,i+1);
         }
         return result;
     }
