f373d20285fe78a814db2705dca49b0748d43904
Potato <potatomichael@gmail.com>
2022-08-29 19:27:50 +1000

    new

--- 75 Ace/[279]Perfect Squares.java
+++ 75 Ace/[279]Perfect Squares.java
@@ -2,14 +2,16 @@ class Solution {
     int[] memo;
     public int numSquares(int n) {
         memo = new int[n + 1];
-        Arrays.fill(memo, -1);
         memo[1] = 1;
         return numSq(n);
     }
     public int numSq(int n) {
-        if (n == 0) return 0;
-        if (memo[n] != -1) return memo[n];
+        if (memo[n] != 0) return memo[n];
         int i = (int)Math.sqrt(n), result = n;
+        if (i * i == n) {
+            memo[n] = 1;
+            return 1;
+        }
         while (i != 0) {
             result = Math.min(result, 1 + numSq(n - i * i));
             i--;
