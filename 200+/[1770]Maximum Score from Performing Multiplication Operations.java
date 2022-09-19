69df4c8d85cc00851c3c90bec7732f076cce7e9e
Potato <potatomichael@gmail.com>
2022-09-19 20:56:03 +1000

    new

--- 200+/[1770]Maximum Score from Performing Multiplication Operations.java
+++ 200+/[1770]Maximum Score from Performing Multiplication Operations.java
@@ -2,21 +2,20 @@ class Solution {
 
     int m, n;
     int[][] memo;
-    int[] _nums, _multipliers;
+    int[] nums, multipliers;
     public int maximumScore(int[] nums, int[] multipliers) {
         m = multipliers.length;
         n = nums.length;
-        _nums = nums;
-        _multipliers = multipliers;
-        int left = 0, right = n - 1;
+        this.nums = nums;
+        this.multipliers = multipliers;
         memo = new int[m][m];
-        return dp(left, right, 0);
+        return dp(0, n - 1, 0);
     }
 
     int dp(int left, int right, int k){
-        if (k == m - 1) return Math.max(_nums[left] * _multipliers[k], _nums[right] * _multipliers[k]) ;
+        if (k == m - 1) return Math.max(nums[left] * multipliers[k], nums[right] * multipliers[k]) ;
         if (memo[left][k] != 0) return memo[left][k];
-        memo[left][k] = Math.max(dp(left + 1, right, k + 1) + _nums[left] * _multipliers[k], dp(left, right - 1, k + 1) + _nums[right] * _multipliers[k]);
+        memo[left][k] = Math.max(dp(left + 1, right, k + 1) + nums[left] * multipliers[k], dp(left, right - 1, k + 1) + nums[right] * multipliers[k]);
         return memo[left][k];
     }
 
